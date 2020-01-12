ce5940f093035417ef5a4a9337711ab2f3c66ab4
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-01-12 22:48:46 +0800

    v1:add turnRight()

--- src/main/java/com/itutry/v1/MarsRover.java
+++ src/main/java/com/itutry/v1/MarsRover.java
@@ -50,4 +50,16 @@ public class MarsRover {
       direction = "N";
     }
   }
+
+  public void turnRight() {
+    if (direction.equals("N")) {
+      direction = "E";
+    } else if (direction.equals("E")) {
+      direction = "S";
+    } else if (direction.equals("S")) {
+      direction = "W";
+    } else if (direction.equals("W")) {
+      direction = "N";
+    }
+  }
 }
--- src/test/java/com/itutry/v1/MarsRoverTest.java
+++ src/test/java/com/itutry/v1/MarsRoverTest.java
@@ -104,4 +104,48 @@ public class MarsRoverTest {
     Assert.assertThat(rover.getY(), is(0));
     Assert.assertThat(rover.getDirection(), is("N"));
   }
+
+  @Test
+  public void turnRight_should_E_for_direction_N() {
+    MarsRover rover = new MarsRover();
+    rover.init(0, 0, "N");
+    rover.turnRight();
+
+    Assert.assertThat(rover.getX(), is(0));
+    Assert.assertThat(rover.getY(), is(0));
+    Assert.assertThat(rover.getDirection(), is("E"));
+  }
+
+  @Test
+  public void turnLeft_should_S_for_direction_E() {
+    MarsRover rover = new MarsRover();
+    rover.init(0, 0, "E");
+    rover.turnRight();
+
+    Assert.assertThat(rover.getX(), is(0));
+    Assert.assertThat(rover.getY(), is(0));
+    Assert.assertThat(rover.getDirection(), is("S"));
+  }
+
+  @Test
+  public void turnLeft_should_W_for_direction_S() {
+    MarsRover rover = new MarsRover();
+    rover.init(0, 0, "S");
+    rover.turnRight();
+
+    Assert.assertThat(rover.getX(), is(0));
+    Assert.assertThat(rover.getY(), is(0));
+    Assert.assertThat(rover.getDirection(), is("W"));
+  }
+
+  @Test
+  public void turnLeft_should_N_for_direction_W() {
+    MarsRover rover = new MarsRover();
+    rover.init(0, 0, "W");
+    rover.turnRight();
+
+    Assert.assertThat(rover.getX(), is(0));
+    Assert.assertThat(rover.getY(), is(0));
+    Assert.assertThat(rover.getDirection(), is("N"));
+  }
 }

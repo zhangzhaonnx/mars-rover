fbf9adfb4f4e969b0d8b8aafa01ad9d737909e28
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-01-13 21:57:14 +0800

    v2:shoud move success, refactor

--- src/main/java/com/itutry/v2/MoveCommand.java
+++ src/main/java/com/itutry/v2/MoveCommand.java
@@ -1,26 +1,25 @@
 package com.itutry.v2;
 
-import static com.itutry.v2.Direction.E;
-import static com.itutry.v2.Direction.N;
-import static com.itutry.v2.Direction.S;
-import static com.itutry.v2.Direction.W;
-
 public class MoveCommand implements Command {
 
   @Override
   public RoverState execute(RoverState state) {
-    if (state.getFacing() == N) {
-      return new RoverState(state.getX(), state.getY() + 1, state.getFacing());
+    int x = state.getX();
+    int y = state.getY();
+    switch (state.getFacing()) {
+      case N:
+        y += 1;
+        break;
+      case E:
+        x += 1;
+        break;
+      case S:
+        y -= 1;
+        break;
+      case W:
+        x -= 1;
+        break;
     }
-    if (state.getFacing() == E) {
-      return new RoverState(state.getX() + 1, state.getY(), state.getFacing());
-    }
-    if (state.getFacing() == S) {
-      return new RoverState(state.getX(), state.getY() - 1, state.getFacing());
-    }
-    if (state.getFacing() == W) {
-      return new RoverState(state.getX() - 1, state.getY(), state.getFacing());
-    }
-    return null;
+    return new RoverState(x, y, state.getFacing());
   }
 }
